5f1f581ee19a774524cd1f17d2f7cf57fe4e1cde
root <root@localhost.localdomain>
2012-11-14 15:54:23 +0800

    update spider

--- spider.rb
+++ spider.rb
@@ -5,18 +5,18 @@ require 'nokogiri'
 require 'spidr'
 
 class Spider
+	def run()
+		target_site = 'http://top.iqiyi.com/dianying.html'
+		Spidr.site(target_site) do |spider|
+			spider.every_url { |url| get_link url }
+		end
+	end
+
 	def get_link(uri)
 		doc = Nokogiri::HTML(open(uri))
 		doc.css('.txt > a').each do |link|
 			p link.content
 			p link['href']
-		end
-	end
-
-	def run()
-		target_site = 'http://top.iqiyi.com/dianying.html'
-		Spidr.site(target_site) do |spider|
-			spider.every_url { |url| get_link url }
 		end
 	end
 end
