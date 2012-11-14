c2c995f15a1b98f34d2e3bd246f5fbc7fdeb84b7
root <root@localhost.localdomain>
2012-11-14 15:51:25 +0800

    add spider

---
+++ spider.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+#encoding: utf-8
+require 'open-uri'
+require 'nokogiri'
+require 'spidr'
+
+class Spider
+	def get_link(uri)
+		doc = Nokogiri::HTML(open(uri))
+		doc.css('.txt > a').each do |link|
+			p link.content
+			p link['href']
+		end
+	end
+
+	def run()
+		target_site = 'http://top.iqiyi.com/dianying.html'
+		Spidr.site(target_site) do |spider|
+			spider.every_url {
+				|url| get_link url
+			}
+		end
+	end
+end
+
+s = Spider.new
+s.run()
